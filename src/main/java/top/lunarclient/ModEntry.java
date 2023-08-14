b3273f1b5a5ae835fbacb6ed47a83baec57c9004
CubeWhy <duck_chenmy1903@163.com>
2023-08-14 13:18:59 +0800

    pkx

--- src/main/java/top/lunarclient/ModEntry.java
+++ src/main/java/top/lunarclient/ModEntry.java
@@ -1,16 +1,12 @@
 package top.lunarclient;
 
 import org.cubewhy.lunarcn.loader.api.ModInitializer;
-import top.lunarclient.proxy.ProxyMain;
 
 @SuppressWarnings("unused")
 public class ModEntry implements ModInitializer {
-    public static final Thread proxyThread = new Thread(new ProxyMain());
 
     @Override
     public void preInit() {
         System.out.println("[LunarHPS] Init!");
-        System.out.println("Start proxy");
-        proxyThread.start();
     }
 }
